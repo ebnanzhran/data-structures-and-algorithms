2ae002757aeade946ae8a0e7d72a180f18b0f327
ebnanzhran <bnanzhran515@gmail.com>
2022-07-05 11:11:31 +0300

    done

--- javascript/trees/binary.js
+++ javascript/trees/binary.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const Node = require('./node');
+
 class BinaryTree {
   constructor(root = null) {
     this.root = root;
@@ -49,6 +51,68 @@ class BinaryTree {
     traverse(this.root);
     return result;
   }
+  
+  Max() {
+    let max = 0;
+    let traverse = (node) => {
+      if (node.value > max) {
+        max = node.value;
+      }
+      if (node.left) {
+        traverse(node.left);
+      }
+      if (node.right) {
+        traverse(node.right);
+      }
+    }
+    traverse(this.root);
+    return max;
+  }
 }
 
-module.exports = BinaryTree;
+class BinaryTreeSearch extends BinaryTree {
+  constructor(root = null) {
+    super(root);
+  }
+
+  Add(value) {
+    let current = this.root;
+    if (!current) {
+      this.root = new Node(value);
+    }
+    else {
+      while (current) {
+        if (value < current.value) {
+          if (!current.left) {
+            current.left = new Node(value);
+            break;
+          }
+          current = current.left;
+        }
+        else {
+          if (!current.right) {
+            current.right = new Node(value);
+            break;
+          }
+          current = current.right;
+        }
+      }
+    }
+  }
+
+  Contains(value) {
+    let current = this.root;
+    while (current) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+module.exports = { BinaryTreeSearch, BinaryTree };
